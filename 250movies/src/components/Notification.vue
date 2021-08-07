0e5425993f5458256fd1fccc1c1e9ec79a023b42
validol4ik <validol4iknew@gmail.com>
2021-08-08 00:21:37 +0300

    added movies src

---
+++ 250movies/src/components/Notification.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "Notification",
+  computed: {
+    ...mapGetters(["messagePool"]),
+  },
+  watch: {
+    messagePool: "showNotification",
+  },
+  methods: {
+    showNotification({ variant, msg, title }) {
+      this.$bvToast.toast(msg, {
+        title: title,
+        variant: variant,
+        solid: true,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
